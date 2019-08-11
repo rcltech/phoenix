2c44957a918f18488d3ce5fa492cee0f3a54ed1f
seanchok <seanchok@gmail.com>
2019-08-12 00:51:09 +0800

    add sports team schema
    #9

--- src/schema.ts
+++ src/schema.ts
@@ -10,29 +10,37 @@ const typeDefs = gql`
         lastname: String
         roomno: String
     }
-    
+
     type Washer {
         id: Int
         status: String
         timeElapsed: String
         timeRemaining: String
     }
-    
+
     type Society {
         name: String
         members: [User]
+    }
+    type SportsTeam {
+      name: String
+      captain: String
+      members: [User]
+      events: [String]
+      contact: String
     }
 
     type Query {
         user(id: ID!): User
         society(id: ID!): Society
         washer(id: ID!): Washer
+        sportsTeam(id: ID!): SportsTeam
     }
-    
+
     type Mutation {
         addUser(newUser: NewUser ): User
     }
-    
+
     input NewUser {
         username: String
         email: String
