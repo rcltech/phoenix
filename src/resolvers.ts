e70f61b29c629943ac177ec75e883213715299a7
utkarsh867 <utkarsh867@gmail.com>
2019-10-18 02:05:51 +0800

    #19: Adds ability to query bookings

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,12 +1,15 @@
 import { createUser, deleteUser, login, register } from './mutations/user';
 import {user, me} from "./query/user";
 import washer from "./query/washer";
+import {bookings, rooms} from "./query/bookings";
 
 const resolvers = {
     Query: {
         user,
         me,
-        washer
+        washer,
+        bookings,
+        rooms
     },
     Mutation: {
         createUser,
@@ -14,6 +17,14 @@ const resolvers = {
         login,
         register
     },
+    Booking: {
+        room(parent, _ , ctx){
+            return ctx.prisma.booking({id: parent.id}).room()
+        },
+        user(parent, _ , ctx){
+            return ctx.prisma.booking({id: parent.id}).user()
+        }
+    }
 };
 
 export default resolvers;
