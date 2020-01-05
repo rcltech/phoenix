de58ff1a1c49421b7cb01cc9ab9475b5387c491c
Sean Chok <seanchok@gmail.com>
2020-01-05 20:21:23 +0800

    order bookings by start in ascending (#29)

--- src/query/bookings.ts
+++ src/query/bookings.ts
@@ -1,5 +1,5 @@
 const bookings = async (parent, {data} , ctx) => {
-  return ctx.prisma.bookings({where: data})
+  return ctx.prisma.bookings({where: data, orderBy: 'start_ASC'})
 };
 
 const rooms = (parent, {data}, ctx) =>
