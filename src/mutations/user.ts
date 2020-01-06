4ee12aa71afaf02294cc01d406d8c054feb31226
utkarsh867 <utkarsh867@gmail.com>
2020-01-06 18:22:28 +0800

    Adds functionality for user sessions

--- src/mutations/user.ts
+++ src/mutations/user.ts
@@ -1,6 +1,6 @@
 import * as env from "dotenv";
-import { JWT, OAuth2Client, VerifyIdTokenOptions } from "google-auth-library";
-import { User } from "../generated/prisma-client";
+import { OAuth2Client } from "google-auth-library";
+import { User, UserSessions } from "../generated/prisma-client";
 import { TokenPayload } from "google-auth-library/build/src/auth/loginticket";
 import { generateToken } from "../utils/authToken";
 
@@ -26,27 +26,35 @@ const createUser = (
 const deleteUser = (parent, { username }, ctx) =>
   ctx.prisma.deleteUser({ username });
 
-const login = async (parent, {}, ctx) => {
+const login = async (parent, args, ctx) => {
   try {
-    await client.verifyIdToken({
+    const ticket = await client.verifyIdToken({
       idToken: ctx.token,
       audience: process.env.GOOGLE_CLIENT_ID,
     });
+    const payload: TokenPayload = ticket.getPayload();
+    // Check if the user email is an HKU email address
+    if (payload && payload.hd !== "connect.hku.hk")
+      return { token: null, login_status: false, register: false };
+    const user: User = await ctx.prisma.user({ email: payload.email });
+    // If the user does not exist, return that the user needs to be registered
+    if (user === null)
+      return { token: null, login_status: false, register: true };
+    // If the user is valid, then register a user session and return a to
+    const userSession : UserSessions = await ctx.prisma.createUserSessions({
+      user: {
+        connect: {
+          id: user.id
+        },
+      },
+    });
+    console.log(userSession);
+    const jwtToken = generateToken(userSession);
+    return { token: jwtToken, login_status: true, register: false };
   } catch (e) {
+    console.log(e);
     return { token: null, login_status: false, register: false };
   }
-  const ticket = await client.verifyIdToken({
-    idToken: ctx.token,
-    audience: process.env.GOOGLE_CLIENT_ID,
-  });
-  const payload: TokenPayload = ticket.getPayload();
-  if (payload && payload.hd !== "connect.hku.hk")
-    return { token: null, login_status: false, register: false };
-  const user: User = await ctx.prisma.user({ email: payload.email });
-  if (user === null)
-    return { token: null, login_status: false, register: true };
-  const jwtToken = generateToken(user);
-  return { token: jwtToken, login_status: true, register: false };
 };
 
 const register = async (parent, { user }, ctx) => {
