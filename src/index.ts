f4c1754f8f079948ab6399d6e57902f5f72dba93
utkarsh867 <utkarsh867@gmail.com>
2020-01-07 21:49:05 +0800

    Adds a PORT environment variable handler

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import * as env from "dotenv";
 env.config();
 
-import server from "./server"
+import server from "./server";
 const express = require("express");
 const app = express();
 
-server.applyMiddleware({app});
+const PORT = process.env.PORT || 4000;
+
+server.applyMiddleware({ app });
 
 app.get("/", (req, res) => {
-    res.send("OK").status(200);
+  res.send("OK").status(200);
 });
 
-app.listen({port: 4000} , () => {
-   console.log("Server started at port 4000");
+app.listen({ port: PORT }, () => {
+  console.log(`Server started at port ${PORT}`);
 });
