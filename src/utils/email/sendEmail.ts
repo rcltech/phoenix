65d2d194c0155df1f6b89cff3949cb7a059e4a6a
Welvin <wewe2712@connect.hku.hk>
2020-01-10 02:35:35 +0800

    Send email using ejs template

---
+++ src/utils/email/sendEmail.ts
@@ -0,0 +1,43 @@
+import * as env from "dotenv";
+import nodemailer from "nodemailer";
+import moment from "moment";
+import ejs from 'ejs';
+env.config();
+
+const transporter = nodemailer.createTransport({
+  service: "Yandex",
+  auth: {
+    user: "owl@rctech.club",
+    pass: process.env.NODEMAILER_PASSWORD,
+  },
+});
+
+export const sendEmail = ({ user, booking, room }) => {
+  const { email, first_name } = user;
+  const { start, end } = booking;
+  const { number, name } = room;
+
+  const room_detail = `${name} (${number})`;
+  const date = moment(start).format("Do MMM YYYY");
+  const start_time = moment(start).format("h:mm");
+  const end_time = moment(end).format("h:mm a");
+  const emailTemplateData = { first_name, room_detail, date, start_time, end_time };
+  
+  const ejsFilePath = '/../../../src/utils/email/emailTemplate.ejs';
+  ejs.renderFile(__dirname + ejsFilePath, emailTemplateData, (err, data) => {
+    if(err){
+      console.log(err);
+    }else{
+      const mailOptions = {
+        from: "owl@rctech.club",
+        to: email,
+        subject: "Owl booking confirmation",
+        html: data
+      };
+    
+      transporter.sendMail(mailOptions, (err, info) => {
+        err ? console.log(err) : console.log(info.response);
+      });
+    }
+  });
+};
