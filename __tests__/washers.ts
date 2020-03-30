94b75f00985b2a09b07404f3cd04fb3db4c83a6c
seanchok <seanchok@gmail.com>
2020-03-31 00:26:43 +0800

    rewrite tests for washers
    test query and update mutation

--- __tests__/washers.ts
+++ __tests__/washers.ts
@@ -1,21 +1,22 @@
 import * as env from "dotenv";
+import { BatchPayload, prisma, Washer } from "../src/generated/prisma-client";
+import { GraphQLResponse } from "apollo-server-types";
+import { createTestClient } from "apollo-server-testing";
+import { createTestServerWithToken } from "./utils/server";
+
 env.config();
 
-import { BatchPayload, prisma, Washer } from "../src/generated/prisma-client";
-import { createTestClient } from "apollo-server-testing";
-import testServer from "../src/server";
-
-const createTestWasher = (washer): Promise<Washer> =>
-  prisma.createWasher(washer);
+const createTestWasher = (washer: {
+  id: string;
+  in_use: boolean;
+}): Promise<Washer> => prisma.createWasher(washer);
 
 const deleteTestWashers = (): Promise<BatchPayload> =>
   prisma.deleteManyWashers({});
 
 const testWasher = {
   id: "1",
-  status: "IDLE",
-  time_elapsed: "0",
-  time_remaining: "0",
+  in_use: true,
 };
 
 beforeAll(async () => {
@@ -24,21 +25,53 @@ beforeAll(async () => {
 
 describe("the graphql washers api", () => {
   test("returns the status of the washing machines", async () => {
+    // create a test server with token
+    const testServer = createTestServerWithToken("some_token");
+    // create a test client connected to the test server
     const client = createTestClient(testServer);
+
     await deleteTestWashers();
     await createTestWasher(testWasher);
-    const washerQuery = `{
-            washer(id: ${testWasher.id}) {
-                id
-                status
-                time_elapsed
-                time_remaining
-            }
-        }`;
-    const queryResponse = await client.query({ query: washerQuery });
-    expect(queryResponse.data).toEqual({
-      washer: testWasher,
+
+    // query and check response
+    const query = `{
+      washers {
+        id
+        in_use
+      }
+    }`;
+    const response: GraphQLResponse = await client.query({ query });
+    expect(response.data).toEqual({
+      washers: [testWasher],
     });
+
+    await deleteTestWashers();
+  });
+
+  test("updates the status of a machine", async () => {
+    // create a test server with token
+    const testServer = createTestServerWithToken("some_token");
+    // create a test client connected to the test server
+    const client = createTestClient(testServer);
+
+    await deleteTestWashers();
+    await createTestWasher(testWasher);
+
+    // update and check response
+    const mutation = `{
+      updateWasher(id: ID!, in_use: Boolean!) {
+        id
+        in_use
+      }
+    }`;
+    const variables = { id: "1", in_use: false };
+    const response: GraphQLResponse = await client.mutate({
+      mutation,
+      variables,
+    });
+    console.log(response);
+    expect(response.data).toEqual({ washer: { id: "1", in_use: false } });
+
     await deleteTestWashers();
   });
 });
