ad3c6b52c89ea6c17ae5667a8313ce18fa02d8b9
Sean Chok <seanchok@gmail.com>
2020-04-15 14:40:43 +0100

    add token verification for updateWasher mutation (#60)

--- src/mutations/washers.ts
+++ src/mutations/washers.ts
@@ -1,6 +1,18 @@
 import { Washer } from "../generated/prisma-client";
+import jwt from "jsonwebtoken";
+import assert from "assert";
 
 const updateWasher = async (parent, { id, in_use }, ctx): Promise<Washer> => {
+  try {
+    const secretString: string = jwt.verify(
+      ctx.token,
+      process.env.SLS_SECRET
+    ) as string;
+    assert.strictEqual(secretString, "sls to phoenix");
+    // same string is sent from sls lambda function
+  } catch (e) {
+    return null;
+  }
   return ctx.prisma.updateWasher({
     data: {
       in_use,
