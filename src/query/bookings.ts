aad560f11d8ac3b27f8963da7869803c5ca86fee
welvin21 <wewe2712@connect.hku.hk>
2020-05-09 16:14:39 +0800

    Add option to query bookings with start time parameter

--- src/query/bookings.ts
+++ src/query/bookings.ts
@@ -1,7 +1,18 @@
 import { Booking, FragmentableArray, Room } from "../generated/prisma-client";
+import moment from "moment";
 
-export const bookings = (parent, { data }, ctx): FragmentableArray<Booking> => {
-  return ctx.prisma.bookings({ where: data, orderBy: "start_ASC" });
+export const bookings = (
+  parent,
+  { data, start },
+  ctx
+): FragmentableArray<Booking> => {
+  if (moment(start, moment.defaultFormat).isValid()) {
+    const start_gte: Date = new Date(start);
+    return ctx.prisma.bookings({
+      where: { ...data, start_gte },
+      orderBy: "start_ASC",
+    });
+  } else return ctx.prisma.bookings({ where: data, orderBy: "start_ASC" });
 };
 
 export const rooms = (parent, { data }, ctx): FragmentableArray<Room> =>
