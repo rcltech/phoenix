ab32d775687cee65639f9b77e9c50361a3e077df
welvin21 <wewe2712@connect.hku.hk>
2020-05-09 16:37:21 +0800

    Add start option for events query

--- src/query/events.ts
+++ src/query/events.ts
@@ -1,5 +1,16 @@
 import { Event, FragmentableArray } from "../generated/prisma-client";
+import moment from "moment";
 
-export const events = (parent, { data }, ctx): FragmentableArray<Event> => {
-  return ctx.prisma.events({ where: data, orderBy: "start_ASC" });
+export const events = (
+  parent,
+  { data, start },
+  ctx
+): FragmentableArray<Event> => {
+  if (moment(start, moment.defaultFormat).isValid()) {
+    const start_gte: Date = new Date(start);
+    return ctx.prisma.events({
+      where: { ...data, start_gte },
+      orderBy: "start_ASC",
+    });
+  } else return ctx.prisma.events({ where: data, orderBy: "start_ASC" });
 };
