ccc3eac550e9f7a49832f23688e47e51975c2590
utkarsh867 <utkarsh867@gmail.com>
2020-05-14 12:45:57 +0530

    Removes irrelevant comment about the server

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,6 @@ import resolvers from "./resolvers";
 import { applyMiddleware } from "graphql-middleware";
 import { permissions } from "./shield/permissions";
 
-/**
- * @author utkarsh867
- * Note that the server instance used in the tests is not the same. This means
- * that when there is a change to the instance of the server underneath, make
- * sure to make the same changes in the tests.
- */
-
 const server = (context): ApolloServer =>
   new ApolloServer({
     schema: applyMiddleware(
