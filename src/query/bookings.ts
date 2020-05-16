b86363d59d71292ad69852761e0295010f9f49c7
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-05-16 17:45:17 +0800

    Add start option for bookings and events queries (#67)
    * Add option to query bookings with start time parameter
    
    * Add start option for events query
    
    * Fixes merge conflicts
    
    * Makes requested changes
    
    * Fixes travis build error by changing test variables
    
    * Changes variable name

--- src/query/bookings.ts
+++ src/query/bookings.ts
@@ -1,7 +1,18 @@
 import { Booking, FragmentableArray, Room } from "../generated/prisma-client";
+import moment from "moment";
 
-export const bookings = (parent, { data }, ctx): FragmentableArray<Booking> => {
-  return ctx.prisma.bookings({ where: data, orderBy: "start_ASC" });
+export const bookings = (
+  parent,
+  { data, start_limit },
+  ctx
+): FragmentableArray<Booking> => {
+  const end_gte: Date = moment(start_limit, moment.defaultFormat).isValid()
+    ? new Date(start_limit)
+    : new Date();
+  return ctx.prisma.bookings({
+    where: { ...data, end_gte },
+    orderBy: "start_ASC",
+  });
 };
 
 export const rooms = (parent, { data }, ctx): FragmentableArray<Room> =>
--- src/query/events.ts
+++ src/query/events.ts
@@ -1,5 +1,16 @@
 import { Event, FragmentableArray } from "../generated/prisma-client";
+import moment from "moment";
 
-export const events = (parent, { data }, ctx): FragmentableArray<Event> => {
-  return ctx.prisma.events({ where: data, orderBy: "start_ASC" });
+export const events = (
+  parent,
+  { data, start_limit },
+  ctx
+): FragmentableArray<Event> => {
+  const end_gte: Date = moment(start_limit, moment.defaultFormat).isValid()
+    ? new Date(start_limit)
+    : new Date();
+  return ctx.prisma.events({
+    where: { ...data, end_gte },
+    orderBy: "start_ASC",
+  });
 };
