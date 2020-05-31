56f9c303d6276f86093bc305a6cfe78047589b88
Utkarsh Goel <utkarsh867@users.noreply.github.com>
2020-05-31 21:39:44 +0530

    Optimises build stage for docker image (#69)
    * makes dist independent of src
    
    * Fixes the gql file for generated

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM node:10-alpine
 WORKDIR usr/src/app
 COPY package.json .
 RUN npm install
-COPY . .
+COPY dist dist
 EXPOSE 4000
 CMD ["npm", "start"]
---
+++ postbuild.js
@@ -0,0 +1,14 @@
+const exec = require("child_process").exec;
+const type = require("os").type;
+
+const operatingSystem = type();
+
+if (operatingSystem === "Linux")
+  exec(
+    "cp src/schema.graphql src/prisma.graphql ./dist && sed -i 's/src/dist/g' ./dist/schema.js"
+  );
+else if (operatingSystem === "Darwin")
+  exec(
+    "cp src/schema.graphql src/prisma.graphql ./dist && sed -i '' 's+src/schema.graphql+dist/schema.graphql+g' ./dist/schema.js"
+  );
+else throw new Error("Unsupported OS found: " + operatingSystem);
