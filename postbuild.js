4f8068fda2b14e72ff63cd2b29fd41c0dd50195c
seanchok <seanchok@gmail.com>
2021-04-11 12:19:40 +0800

    fix postbuild script

--- postbuild.js
+++ postbuild.js
@@ -5,10 +5,10 @@ const operatingSystem = type();
 
 if (operatingSystem === "Linux")
   exec(
-    "cp src/schema.graphql src/prisma.graphql ./dist && sed -i 's/src/dist/g' ./dist/schema.js"
+    "cp src/schema.graphql src/prisma.legacy.graphql ./dist && sed -i 's/src/dist/g' ./dist/schema.js"
   );
 else if (operatingSystem === "Darwin")
   exec(
-    "cp src/schema.graphql src/prisma.graphql ./dist && sed -i '' 's+src/schema.graphql+dist/schema.graphql+g' ./dist/schema.js"
+    "cp src/schema.graphql src/prisma.legacy.graphql ./dist && sed -i '' 's+src/schema.graphql+dist/schema.graphql+g' ./dist/schema.js"
   );
 else throw new Error("Unsupported OS found: " + operatingSystem);
